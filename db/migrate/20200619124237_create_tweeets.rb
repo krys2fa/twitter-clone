29d16da916b2fd30793e1d73471cb9137a56b29b
Christopher Amanor <kriskit7@gmail.com>
2020-06-22 09:44:54 +0000

    Add layout and markup and devise user model

--- db/migrate/20200619124237_create_tweeets.rb
+++ db/migrate/20200619124237_create_tweeets.rb
@@ -2,6 +2,7 @@ class CreateTweeets < ActiveRecord::Migration[5.2]
   def change
     create_table :tweeets do |t|
       t.text :tweeet
+      t.title :tweeet, :string
 
       t.timestamps
     end
